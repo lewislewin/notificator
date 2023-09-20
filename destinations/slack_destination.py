ad0c5964da31666418aa9fbff9a100aa7b14f3b1
Lewis Lewin <lewis@lewislewin.com>
2023-09-20 20:49:55 +0100

    able to send a message to slack

--- destinations/slack_destination.py
+++ destinations/slack_destination.py
@@ -1,7 +1,26 @@
 from .abstract_destination import AbstractDestination
+from models.notification import Notification
+import os
+import requests
+
 
 class SlackDestination(AbstractDestination):
-    def send_notification(self, request):
-        print('Sending notification to Slack!')
-        print(request)
+    def __init__(self):
+        
+        pass
+
+    def send_notification(self, notification: Notification):
+        slack_url = os.environ.get('SLACK_URL')
+        slack_message = self.format_message(notification)
+
+        response = requests.post(slack_url, json={'text': slack_message})
+
+        if response.status_code != 200:
+            raise ValueError(
+                f'Request to Slack returned an error {response.status_code}, {response.text}'
+            )
+        
         return True
+    
+    def format_message(self, notification: Notification) -> str:
+        return f'{notification.title}\n{notification.message}'
--- sources/zendesk_source.py
+++ sources/zendesk_source.py
@@ -1,8 +1,16 @@
 from .abstract_source import AbstractSource
+from models.notification import Notification
 
 class ZendeskSource(AbstractSource):
-    def process_data(self, request):
-        pass
+    def process_data(self, request) -> Notification:
+        data = request['ticket']
+        return Notification(
+            id = data['id'],
+            source = 'Zendesk',
+            title = data['subject'],
+            message = data['description'],
+            priority = data['priority']
+        )
 
     def verify_request(self, request):
         pass
